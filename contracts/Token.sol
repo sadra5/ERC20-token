18fafcef0f4e3df882255fac53f42d10566b1cfd
Sadra Rezaei <mr.sadra83@gmail.com>
2023-10-26 23:26:22 +0330

    first commit

---
+++ contracts/Token.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/toen/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/toen/ERC20/ERC20Capped.sol";
+
+contract myToken is  ERC20Capped {
+
+    constructor (string memory name_, string memory symbol_, uint initailSupply, uint256 cap) ERC20 (name_, symbol_) ERC20Capped(cap * (10 ** decimals )) {
+        _mint(msg.sender, initailSupply * (10 ** decimals));
+    }
+} 
