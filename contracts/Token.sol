a62a8813ebc48417c67579e656758bc42bc78bcb
Sadra Rezaei <mr.sadra83@gmail.com>
2023-10-27 00:24:30 +0330

    last commit

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,8 +5,9 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/toen/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/toen/ERC20/ERC20Capped.sol";
+import "@openzeppelin/contracts/toen/ERC20/ERC20Burnable.sol";
 
-contract myToken is  ERC20Capped {
+contract myToken is  ERC20Capped, ERC20Burnable {
 
     constructor (string memory name_, string memory symbol_, uint initailSupply, uint256 cap) ERC20 (name_, symbol_) ERC20Capped(cap * (10 ** decimals )) {
         _mint(msg.sender, initailSupply * (10 ** decimals));
