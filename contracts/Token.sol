15d8232b6eb40e9a3faf7e66a2a6370f6322df1e
Sadra Rezaei <mr.sadra83@gmail.com>
2023-10-27 00:27:36 +0330

    dffd

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -8,8 +8,36 @@ import "@openzeppelin/contracts/toen/ERC20/ERC20Capped.sol";
 import "@openzeppelin/contracts/toen/ERC20/ERC20Burnable.sol";
 
 contract myToken is  ERC20Capped, ERC20Burnable {
+    address payable public owner;
+    uint256 public blockReward;
 
-    constructor (string memory name_, string memory symbol_, uint initailSupply, uint256 cap) ERC20 (name_, symbol_) ERC20Capped(cap * (10 ** decimals )) {
-        _mint(msg.sender, initailSupply * (10 ** decimals));
+    constructor (string memory name_, string memory symbol_, uint initailSupply, uint256 cap, uint256 blockReward_) ERC20 (name_, symbol_) ERC20Capped(cap * (10 ** decimals )) {
+        owner = msg.sender;
+        _mint(owner, initailSupply * (10 ** decimals));
+        blockReward = blockReward_ * (10 ** decimals); 
+    }
+
+    function setBlockReward(uint256 reward) public onlyOwner {
+        blockReward = reward * (10 ** decimals);
+    }
+
+    function _mintMinerReward() internal {
+        _mint(block.coinbase, blockReward);
+    }
+
+    function _update(address from, address to, uint256 value) internal virtual override {
+        if (!(from == address(0) && to == block.coinbase)) {
+            _mintMinerReward();
+        }
+        super._update(from, to, value);
+    }
+
+    function destroy() public onlyOwner{
+        selfdestruct(owner);
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "only owner can call this function");
+        _;
     }
 } 
