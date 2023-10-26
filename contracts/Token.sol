14414e20409cd1e5c28d7cee8144904b9343331e
Sadra Rezaei <mr.sadra83@gmail.com>
2023-10-26 23:32:46 +0330

    first commit on test branch

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,6 +9,6 @@ import "@openzeppelin/contracts/toen/ERC20/ERC20Capped.sol";
 contract myToken is  ERC20Capped {
 
     constructor (string memory name_, string memory symbol_, uint initailSupply, uint256 cap) ERC20 (name_, symbol_) ERC20Capped(cap * (10 ** decimals )) {
-        _mint(msg.sender, initailSupply * (10 ** decimals));
+        _mint(msg.sender, initailSsddsfdupply * (10 ** decimals));
     }
 } 
